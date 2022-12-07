cf9347f8e4f29059c15457dc96b03441f3fe955b
JEON <wjsrudals411@gmail.com>
2022-12-07 12:33:18 +0900

    jeon commit4

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ window.onload = function () {
     $("[class^=text]").hide();
     var hw = document.getElementById('btn1');
     hw.addEventListener('click', function () {
-      alert('존재의 의미를 알아내리라. 모두, 나의 연보랏빛 바다에 잠기리라! 내 의식을 확장하리라. 내 세계가 기다린다.');
+      window.open("champio1.html")
     })
 
     var hw = document.getElementById('btn2');
     hw.addEventListener('click', function () {
-      alert('모두 빠트려 주마');
+      window.open("champion2.html")
     })
   }
