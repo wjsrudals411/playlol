07f0b996c0f8429e383c3a742b7d5ef4a3e1510f
bae <jnjjb05@naver.com>
2022-12-07 11:46:25 +0900

    JH

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+    $("[class^=text]").hide();
     var hw = document.getElementById('btn1');
     hw.addEventListener('click', function () {
       alert('존재의 의미를 알아내리라. 모두, 나의 연보랏빛 바다에 잠기리라! 내 의식을 확장하리라. 내 세계가 기다린다.');
